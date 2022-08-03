5dd4232664b542820d2cc194e86478282278399e
SIBI RAJ S <84224170+sibisr@users.noreply.github.com>
2022-08-03 06:40:31 +0530

    Update Banner.js

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -16,3 +16,42 @@ const Banner = () => {
 };
 
 export default Banner;
+import React from "react";
+import logo from "../../imgs/logo.png";
+import agent from "../../agent";
+
+const Banner = (props) => {
+  const handleChange = (ev) => {
+    const title = ev.target.value;
+    if (title.length < 3) {
+      return;
+    }
+
+    props.onChangeTitle(
+      title,
+      (page) => agent.Items.byTitle(title, page),
+      agent.Items.byTitle(title)
+    );
+  };
+
+  return (
+    <div className="banner text-white">
+      <div className="container p-4 text-center">
+        <img src={logo} alt="banner" />
+        <div>
+          <span id="get-part">A place to get</span>
+          <input
+            id="search-box"
+            type="text"
+            name="title"
+            placeholder="What is it that you truly desire?"
+            onChange={handleChange}
+          />
+          <span> the cool stuff.</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
